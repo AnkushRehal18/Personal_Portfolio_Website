8a9b84cdffa774965d3be23c3d9f1468a8f6553f
Ankush <rehalankush192@gmail.com>
2024-08-24 18:46:21 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,7 +4,7 @@ import { ProjectCard } from './ProjectCard';
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1 .png";
 import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg3 from '../assets/img/project-img3.png';
 import TrackVisibility from 'react-on-screen';  // Import TrackVisibility if not already
 
 const Projects = () => {
